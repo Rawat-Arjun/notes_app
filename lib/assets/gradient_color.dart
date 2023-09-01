d7ddd61989e4c8ffa8aa91d11944f362b7af4975
Arjun Rawat <arjunrawat12121@gmail.com>
2023-09-01 10:01:16 +0530

    initial commit

---
+++ lib/assets/gradient_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+//login page gradient
+const Color lg1 = Color(0xffFF512F);
+const Color lg2 = Color(0xffDD2476);
+
+//register page gradient
+const Color rg1 = Color(0xff662D8C);
+const Color rg2 = Color(0xffED1E79);
--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,206 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_app/assets/gradient_color.dart';
 
 void main() {
-  runApp(const HomePage());
+  runApp(
+    const LoginApp(),
+  );
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class LoginApp extends StatelessWidget {
+  const LoginApp({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'My Notes',
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NotesApp(),
+      title: 'Login',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      darkTheme: ThemeData.dark(),
+      home: const login_page(),
     );
   }
 }
 
-class NotesApp extends StatefulWidget {
-  const NotesApp({super.key});
+class login_page extends StatefulWidget {
+  const login_page({super.key});
 
   @override
-  State<NotesApp> createState() => _NotesAppState();
+  State<login_page> createState() => _login_pageState();
 }
 
-class _NotesAppState extends State<NotesApp> {
+class _login_pageState extends State<login_page> {
+  final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    MediaQuery.sizeOf(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Notes App'),
-        centerTitle: true,
-      ),
-      body: const Row(
-        children: [
-          Center(
-            child: Text(
-              'Hello World',
+      body: Container(
+        height: double.maxFinite,
+        padding: const EdgeInsets.only(
+          top: 50,
+        ),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [rg1, rg2],
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                const CircleAvatar(
+                  radius: 120,
+                  backgroundImage: AssetImage(
+                    'lib/assets/image/login.jpg',
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(
+                        left: 10,
+                      ),
+                    ),
+                    RichText(
+                      text: const TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'Welcome Back :)',
+                            style: TextStyle(
+                              color: Colors.white70,
+                              fontSize: 25,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: email,
+                        enableSuggestions: false,
+                        keyboardType: TextInputType.emailAddress,
+                        autofillHints: const [AutofillHints.email],
+                        decoration: InputDecoration(
+                          hintText: 'Email Address',
+                          prefixIcon: const Icon(
+                            Icons.account_circle,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        controller: password,
+                        enableSuggestions: false,
+                        obscureText: true,
+                        autofillHints: const [AutofillHints.password],
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          prefixIcon: const Icon(
+                            Icons.vpn_key_sharp,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                CupertinoButton(
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 60,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      color: const Color(0xff662D8C),
+                      borderRadius: BorderRadius.circular(40),
+                      boxShadow: const [
+                        BoxShadow(
+                          blurRadius: 3,
+                          spreadRadius: 0,
+                          color: Color.fromRGBO(
+                            120,
+                            37,
+                            139,
+                            0.25,
+                          ),
+                        ),
+                      ],
+                    ),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+                const Divider(
+                  height: 30,
+                  color: Colors.white30,
+                  thickness: 1,
+                  indent: 10,
+                  endIndent: 10,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                CupertinoButton(
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 60,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.white38,
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: const Text(
+                      'Already an Account',
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
